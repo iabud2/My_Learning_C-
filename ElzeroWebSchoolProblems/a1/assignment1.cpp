be4af6404e53c1b85aef487a8fde21bbe58a8927
iabud20 <abdullah.saeed50.99@gmail.com>
2024-02-13 19:54:35 +0300

    Solving Problems for lvl 1 and 2 in the course

---
+++ ElzeroWebSchoolProblems/a1/assignment1.cpp
@@ -0,0 +1,66 @@
+
+
+// 1
+#include <iostream>
+
+int main()
+{
+    std::cout << "Line one\n";
+    std::cout << "Line Two\n";
+    std::cout << "Line Three\n";
+    return 0;
+}
+
+// 2
+
+/*
+iostream Header File Content
+    -- cout
+    -- cin
+    -- cerr
+    -- clog
+    -- wcin
+    -- wcout
+    -- wcerr
+    -- wclog
+*/
+
+// 3
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3";
+    return 1;
+}
+
+// 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "|=======================|\n";
+    cout << "|== Elzero Web School ==|\n";
+    cout << "|==        C++        ==|\n";
+    cout << "|=======================|";
+    return 2;
+}
+
+// 5
+
+// This Is My First File
+// In Learning Programming
+// Iam Happy <3
+// Iam Dragon
+
+#include <iostream> /* Preprocessor Directive */
+int main()
+{
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n"; /* cout Function */
+    std::cout << "Line Three\n";
+    // Return 0
+    return 5;
+}
